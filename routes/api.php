d3628880df6fa8bfd1ae014c6282516b31fa7bab
sahil kadam <sahil.kadam@homevillegroup.com>
2025-06-11 13:01:46 +0530

    route changes

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\PortfolioController;
 Route::middleware(['throttle:60,1'])->group(function () {
     Route::post('/portfolio', [PortfolioController::class, 'store']);
     // Route::post('/portfolio', [PortfolioController::class, 'scrapeWithPuppeteer']);
-    // Route::get('/portfolio/{username}', [PortfolioController::class, 'show']);
-    Route::get('/portfolio/{username}', [PortfolioController::class, 'scrapeByUsername']);
+    Route::get('/portfolio/{username}', [PortfolioController::class, 'show']);
+    // Route::get('/portfolio/{username}', [PortfolioController::class, 'scrapeByUsername']);
     Route::put('/portfolio/{username}', [PortfolioController::class, 'update']);
     Route::delete('/portfolio/{username}', [PortfolioController::class, 'destroy']);
 });
